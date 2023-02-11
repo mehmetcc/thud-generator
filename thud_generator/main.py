e05deb25683d9255438a60a5bf9a432e9b68933c
mehmetcc <mehmetccm@gmail.com>
2023-02-11 14:03:51 +0300

    add main function

--- thud_generator/main.py
+++ thud_generator/main.py
@@ -4,3 +4,7 @@ from fastapi import FastAPI
 def start():
     """Launched with `poetry run start` at root level"""
     uvicorn.run("thud_generator.api:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    start()
